2cddbae3605761d4944fa05f9aec12a4a6373e8c
geriiy <gheriyaj@gmail.com>
2023-10-16 15:31:03 +0700

    modifikasi1

--- ForKelipatan12.java
+++ ForKelipatan12.java
@@ -6,22 +6,25 @@ public class ForKelipatan12 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        int jumlah, kelipatan, counter;
-        jumlah = 0;
-        counter = 0;
+        int jumlah = 0, kelipatan, counter = 0, average = 0;
+       
 
         System.out.print("Masukkan bilangan kelipatan  (1-9): ");
         kelipatan = scan.nextInt();
 
         for (int i = 1; i <= 50; i++) {
             if (i % kelipatan == 0) {
-                total += i;
+                jumlah += i;
                 counter++;
+                average = jumlah / counter;
+
+
             }
         }
         
         System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
-        System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, total);
+        System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, jumlah);
+        System.out.printf("Rata-rata bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, average);
         
     }
 
